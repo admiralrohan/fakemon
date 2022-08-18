7fb4db06c10aa4307294fa91ebdfba3e17974401
Rohan Gayen <rohangayen@gmail.com>
2022-08-18 17:30:57 +0530

    chore: minor change + add chainlink contracts package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import { Gyms } from "./containers/Gyms";
 import { Header } from "./components/Header";
