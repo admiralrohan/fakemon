1969d62662c05049aee623718f4fd48cd2d23571
Rohan Gayen <rohangayen@gmail.com>
2022-08-18 20:36:41 +0530

    chore(BC): remove `_afterTokenTransfer` NFT hook

--- contracts/Fakemon.sol
+++ contracts/Fakemon.sol
@@ -119,18 +119,6 @@ contract Fakemon is ERC1155 {
         fakemonStats[lastCharacterId] = Stat(7, 5, 3, 0, msg.sender);
     }
 
-    function _afterTokenTransfer(
-        address operator,
-        address from,
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) internal virtual override {
-        super._afterTokenTransfer(operator, from, to, ids, amounts, data);
-        // characterIds[to].push(ids[0]);
-    }
-
     function createNewGym(uint[] memory nftIds) external {
         lastGymId += 1;
 
