9c0313ed7531bbd2190c9b489502da3893a688a5
Rohan Gayen <rohangayen@gmail.com>
2023-04-04 03:53:36 +0530

    fix(FE): not logging in after connecting to wallet
    Due to instant page reload

--- frontend/src/components/AuthProvider/AuthProvider.js
+++ frontend/src/components/AuthProvider/AuthProvider.js
@@ -9,7 +9,12 @@ function AuthProvider(props) {
    * Using this to avoid calling to whole blockchain instance in components. */
   const [walletAddress, setWalletAddress] = React.useState(null);
 
-  const handleAccountsChanged = React.useRef(() => window.location.reload());
+  const handleAccountsChanged = React.useRef(() => {
+    // When connecting to wallet for first time, this function is getting triggered before it has chance to save `signerAddress` in localStorage. So after the page reload it's not automatically connecting to wallet. Leading to bad UX.
+    setTimeout(() => {
+      window.location.reload();
+    }, 100);
+  });
   const handleChainChanged = React.useRef(() => window.location.reload());
 
   const exportedMembers = {
