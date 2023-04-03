7993f1ead3c6c7e217257c087e536964c2df3f77
Rohan Gayen <rohangayen@gmail.com>
2023-04-04 03:55:44 +0530

    perf(FE): only reload if user changes wallet account while logged in
    Unnecessary reloading while logging in for first time
    Will cause extra API requests to Alchemy

--- frontend/src/components/AuthProvider/AuthProvider.js
+++ frontend/src/components/AuthProvider/AuthProvider.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { LOCALSTORAGE_KEY } from "../../utils/utils";
 
 export const AuthContext = React.createContext();
 AuthContext.displayName = "AuthContext";
@@ -10,10 +11,10 @@ function AuthProvider(props) {
   const [walletAddress, setWalletAddress] = React.useState(null);
 
   const handleAccountsChanged = React.useRef(() => {
-    // When connecting to wallet for first time, this function is getting triggered before it has chance to save `signerAddress` in localStorage. So after the page reload it's not automatically connecting to wallet. Leading to bad UX.
-    setTimeout(() => {
-      window.location.reload();
-    }, 100);
+    const savedAddress = window.localStorage.getItem(LOCALSTORAGE_KEY);
+
+    // Will only reload if user changes wallet account while logged in
+    if (savedAddress) window.location.reload();
   });
   const handleChainChanged = React.useRef(() => window.location.reload());
 
